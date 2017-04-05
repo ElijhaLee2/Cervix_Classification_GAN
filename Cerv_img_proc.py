a28d53ee414d8cbcabfa59929c475f9fa2dfdf9c
elijha <fun-star@qq.com>
2017-04-05 22:28:50 +0800

    resize and resave images

--- Cerv_img_proc.py
+++ Cerv_img_proc.py
@@ -2,11 +2,15 @@ import PIL.Image as Image
 import os as os
 import os.path as os_path
 
-train_img_root = '/media/elijha/Software/data/train'
-type_dir_list = os.listdir(train_img_root)
+train_img_input_dir = '/home/elijha/Documents/Data/Cervix/train'
+train_img_output_dir = '/home/elijha/Documents/Data/Cervix/train_converted'
+type_dir_list = os.listdir(train_img_input_dir)
 
 for type_dir in type_dir_list:
-    type_dir = os_path.join(train_img_root, type_dir)
+    output_type_dir = os_path.join(train_img_output_dir,type_dir)
+    if not os_path.isdir(output_type_dir):
+        os.makedirs(output_type_dir)
+    type_dir = os_path.join(train_img_input_dir, type_dir)
     if not os_path.isdir(type_dir):
         continue
     else:
@@ -16,4 +20,8 @@ for type_dir in type_dir_list:
         if not img_dir.split('.')[-1] == 'jpg':
             continue
         else:
-            img = Image.open(os_path.join(type_dir, img_dir))
+            # 读出图片并resize成3000*2500
+            img = Image.open(os_path.join(type_dir, img_dir)).resize([2500,3000])
+            img.save(os_path.join(output_type_dir,img_dir))
+            # img.save('test.bmp')
+            # img = img.show()
